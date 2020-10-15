784244564512e16276461b8848d6d4a4dd16265a
NoelGaspar <waaraya@uc.cl>
2020-10-15 10:16:19 -0300

    version 1.0

--- firmware_code/CIEP_EFMMP/src/main.cpp
+++ firmware_code/CIEP_EFMMP/src/main.cpp
@@ -56,6 +56,7 @@ TODOLIST:
 -Probe DACs of the esp32                 check
 -Probe DHT sensor.
 -Probe pt100 sensor. 
+-ADD automatic temperature control feature
 -Probe OTA.
 
 */ 
@@ -65,32 +66,27 @@ TODOLIST:
     Includes
 -----------------*/
 #include <Arduino.h>
-#include <WiFi.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <HardwareSerial.h>
 #include <ArduinoJson.h>
-#include <SoftwareSerial.h>
 #include <Adafruit_SHT31.h>
-
+#include <DHT.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 /*----------------
     Defines
 -----------------*/
 //PIN DEFINITIONS
-#define LED_PIN         13            //ON BOARD LED PIN
-#define STATUS_LED_PIN  15  //STATUS EXTERNAL LED
-
-//#define SDA_PIN   18  //I2C SDA
-//#define SCK_PIN   19  //I2C SCL
+#define LED_PIN           13 //ON BOARD LED PIN
+#define LED_STATUS_PIN    15 //STATUS LED
+#define HEATER_PIN        25 //DAC OUTPUT PIN
+#define PT100_SENSOR_PIN  18 //PRE CHAMBER TEMPERATURE SENSOR
+#define DHT_SENSOR_PIN    19 //I2C SCL
 
 #define SCL_PIN   34  // SPI SCL
 #define MOSI_PIN  32  // SPI MOSI
 #define CS_PIN    33  // SPI CS
-
-//#define PWM1_PIN  13  //HEATER
-//#define PWM2_PIN  15  //COOLER
-#define HEATER_PIN  25 //DAC OUTPUT PIN
 
 //SENSOR PMS UART
 #define PMS_TX    14  //UART PMS TX
@@ -108,9 +104,12 @@ TODOLIST:
 //SERIAL DEFINITIONS
 #define SerialDBG       Serial
 #define SerialSIM       Serial1
+#define SerialPMS       Serial2
 #define BAUDRATE_DEBUG  9600
 #define BAUDRATE_SIM    9600
 #define BAUDRATE_PMS    9600
+#define PMS5003ST
+//#define PMS5003 //FOR D AND C sensors
 
 // Configure TinyGSM library
 #define TINY_GSM_MODEM_SIM800       // Modem is SIM800
@@ -129,12 +128,15 @@ TODOLIST:
 #define RESPONSE_STOP     "stop_ok"   // RESPONSE TO INCOMMING CMD
 #define TYPE_DATA         "data"      // TYPE OF RESPONSE. DATA: DATA FROM SENSOR
 #define TYPE_RESP         "resp"      // TYPE OF RESPONSE. RESP: RESPONSE TO INCOMMING CMD
+
 #define TIMER_F_SCLK      10000       // TIMER SOURCE CLOCK. IN kHz
-#define TIMER_DEFAULT_TS  4           // TIMER DEFAULT PERIOD. IN SECONDS 
+#define TIMER_DEFAULT_TS  8           // TIMER DEFAULT PERIOD. IN SECONDS 
 #define MAX_PMS_BUFF_SIZE 20          // SIZE OF THE ARRAY OF DATA TO SEND FORM THE PMS
-#define HEATER_LIMIT      80         // SOFTWARE LIMIT FOR THE HEATER. OUTPUT RANGE : 0 .. 255
+#define HEATER_LIMIT      80          // SOFTWARE LIMIT FOR THE HEATER. OUTPUT RANGE : 0 .. 255
+
 //SHT31
 #define SHT31_ADDR        0x44
+#define DHTTYPE           DHT22
 
 //GPRS sim credentials
 const char apn[]      = "freeeway"; // APN from PRIMCARDS
@@ -157,68 +159,88 @@ const char* mqtt_server = "190.121.23.217"; //mqtt IP broker (string)
 /*-----------------
     Instances
 -----------------*/
-//JSON to SEND
-//const size_t capacity_tx = JSON_ARRAY_SIZE(100) + JSON_OBJECT_SIZE(7);
-//DynamicJsonDocument doc_tx(capacity_tx);
+
 char json_tx[1024];
-//JsonArray data = doc.createNestedArray("data");
 
 //JSON to RECIVE
 const size_t capacity_rx = JSON_OBJECT_SIZE(4) + 40;
 DynamicJsonDocument doc_rx(capacity_rx);
-const char* json_rx =  "{\"id\":\"em-ciep-05\",\"cmd\":\"LED\",\"arg1\":48.756,\"arg2\":48.75608}";
+const char* json_rx =  "{\"id\":\"em-ciep-01\",\"cmd\":\"LED\",\"arg1\":48.756,\"arg2\":48.75608}";
 DeserializationError error_rx;
 
 //I2C BUSES 
 TwoWire I2C_BUS = TwoWire(0);
-//TwoWire I2C_BUS = TwoWire(1);
 
 //Client and mqtt
 TinyGsm       modem(SerialSIM);
 TinyGsmClient client(modem);
 PubSubClient  mqtt(client);
 
-//PMS read port
-SoftwareSerial SerialPMS(PMS_RX,PMS_TX);
-
-//timer
-hw_timer_t * timer = NULL;
+//timers 
+hw_timer_t * timer = NULL;    //timer to adquisition of sensors
+hw_timer_t * timerhm = NULL;  //timer to control the heater
 
 //SHT31 sensor
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+//DHT sensor
+DHT dht(DHT_SENSOR_PIN,DHTTYPE);
+
+//oneWire sensor
+OneWire oneWire(PT100_SENSOR_PIN);
+DallasTemperature pt100(&oneWire);
 
 /*----------------------------
     VARIABLES AND STRUCTURES
 -----------------------------*/
 //Structs to save sensor data
-//struct SHT31_data
-//{
-//  bool   enableHeater = false;
-//  float  temp  = 0;
-//  float  rh = 0;
-//};
-//struct SHT31_data ext_sens_data; // 
-
-struct pms5003data
-{
-  uint16_t framelen;
-  uint16_t pm10_standard, pm25_standard, pm100_standard;
-  uint16_t pm10_env, pm25_env, pm100_env;
-  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
-  uint16_t unused;
-  uint16_t checksum;
-};
+#ifdef PMS5003ST
+  struct pms5003data
+  {
+    uint16_t framelen;
+    uint16_t pm10_standard, pm25_standard, pm100_standard;
+    uint16_t pm10_env, pm25_env, pm100_env;
+    uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+    uint16_t pm_fc;
+    uint16_t pm_t;
+    uint16_t pm_h;
+    uint16_t unused;
+    uint16_t error;
+    uint16_t checksum;
+  };
+#endif
+#ifdef PMS5003
+  struct pms5003data
+  {
+    uint16_t framelen;
+    uint16_t pm10_standard, pm25_standard, pm100_standard;
+    uint16_t pm10_env, pm25_env, pm100_env;
+    uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;    
+    uint16_t unused;
+    uint16_t checksum;
+  };
+#endif
 struct pms5003data data;
 
+bool pms_data_ready = false ; // FLAG FOR PMS CORRECT ADQUISITION
+long pms_count      = 0;      // COUNTER FOR CORRECT ADQUIRED DATA
+
+//TIMER vars
 int timer_adqt    = TIMER_DEFAULT_TS;             //VARIABLE TO SET THE TIMER PERIOD. PERIOD = ADQUISITION TIME
 int timer_counter = int(timer_adqt * TIMER_F_SCLK); //VARIABLE TO SET THE MAX COUNT OF THE TIMER COUNTER
 bool timer_flag   = false;
+
+//TIMER2 heater monitoring
+int timerhm_adqt    = TIMER_DEFAULT_TS;             //VARIABLE TO SET THE TIMER PERIOD. PERIOD = ADQUISITION TIME
+int timerhm_counter = int(timer_adqt * TIMER_F_SCLK); //VARIABLE TO SET THE MAX COUNT OF THE TIMER COUNTER
+bool timerhm_flag   = false;
 
 //data buffer 
 uint8_t pms_idx   = 0;                // INDEX OF THE PMS BUFFER ARRAY
 bool    pms_flag  = false;            // SAMPLES ARE READY
 uint16_t PMS_buff[MAX_PMS_BUFF_SIZE]; // PMS BUFFER ARRAY
 
+//sensors vars
 float SHT_temp_buff[MAX_PMS_BUFF_SIZE];
 float SHT_rh_buff[MAX_PMS_BUFF_SIZE];
 float sht_temp_avg  = 0.0;
@@ -233,8 +255,16 @@ float dht_rh_avg    = 0.0;
 float dht_temp      = 0.0;
 float dht_rh        = 0.0;
 
+float PMS_temp_buff[MAX_PMS_BUFF_SIZE];
+float PMS_rh_buff[MAX_PMS_BUFF_SIZE];
+float pms_temp_avg  = 0.0;
+float pms_rh_avg    = 0.0;
+float pms_temp      = 0.0;
+float pms_rh        = 0.0;
+
 //LED
-bool led_state = false;
+bool led_state    = false; // for test LED
+bool led_s_state  = false; // for status LED
 
 //HEATER 
 uint8_t heater_value = 0;     // VARIABLE TO SAVE THE HEATER OUTPUT VALUE. RANGE : 0 TO 255
@@ -261,13 +291,14 @@ void publishSensor(const char *resp);
 void readSensors();
 bool readPMSdata(Stream *s);
 void IRAM_ATTR timerISR();
+void IRAM_ATTR timer_HM_ISR();
 
 
 void setup()
 {
   //Serial
   SerialDBG.begin(BAUDRATE_DEBUG);
-  SerialPMS.begin(BAUDRATE_PMS);
+  SerialPMS.begin(BAUDRATE_PMS, SERIAL_8N1,PMS_RX,PMS_TX);
   delay(15);
 
   //I2C
@@ -287,28 +318,46 @@ void setup()
 
   //onboard LED
   pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_STATUS_PIN, OUTPUT);
+
   digitalWrite(LED_PIN, LOW);
+  digitalWrite(LED_STATUS_PIN, LOW);
+
+  //dac start on 0
+  dacWrite(HEATER_PIN,0);
 
   //default dumy data sensors
   response = RESPONSE_OK;
 
-
+  //Check digital dependenses
   if (!sht31.begin(SHT31_ADDR))
   {
     Serial.println("Couldn't find SHT31");
   }
 
+  dht.begin();
+  delay(150);
+  pt100.begin();
+  delay(150);
+    
   //Set GSM module baud rate and UART pins
   SerialSIM.begin(9600, SERIAL_8N1, MODEM_RX, MODEM_TX);
   delay(3000);
 
   //setup Timer
-  timer = timerBegin(3, 8000, true);    //timer 4, prescaler 8000, counting up
+  timer = timerBegin(3, 8000, true);    //timer 3, prescaler 8000, counting up
   timerAttachInterrupt(timer,&timerISR,true); //params: timer object, pointer to ISR function anddress, mode edge (if false: level mode)
   timerAlarmWrite(timer, timer_counter,true);   // params: timer object, counter_limit, restart counter on top.// to get T= 1s, n=T*f_{timer source clock}
   timerAlarmEnable(timer);              // enable CTC mode
   timerStop(timer);                     //stop timer
-  
+
+//  //setup Timer
+//  timerhm = timerBegin(2, 8000, true);              //timer 2, prescaler 8000, counting up
+//  timerAttachInterrupt(timerhm,&timer_HM_ISR,true); //params: timer object, pointer to ISR function anddress, mode edge (if false: level mode)
+//  timerAlarmWrite(timerhm, timerhm_counter,true);   // params: timer object, counter_limit, restart counter on top.// to get T= 1s, n=T*f_{timer source clock}
+//  timerAlarmEnable(timerhm);              // enable CTC mode
+//  timerStop(timerhm);                     //stop timer
+
   //GSM
   setup_gsm();
 
@@ -325,6 +374,7 @@ void loop()
   mqtt.loop();
   if(timer_flag)
   {
+    SerialDBG.println("get data");
     readSensors();
     timer_flag = false;
   }
@@ -408,15 +458,14 @@ void reconnect()
 
 void mqttCallback(char* topic, byte *payload, unsigned int length)
 {
+  SerialDBG.println("-------new message from broker-----");
+  SerialDBG.print("channel:");
+  SerialDBG.println(topic);
+  SerialDBG.print("data:");
+  SerialDBG.write(payload, length);
+  SerialDBG.println();
 
-    SerialDBG.println("-------new message from broker-----");
-    SerialDBG.print("channel:");
-    SerialDBG.println(topic);
-    SerialDBG.print("data:");
-    SerialDBG.write(payload, length);
-    SerialDBG.println();
-
-    processCmd(payload,length);
+  processCmd(payload,length);
 }
 
 void processCmd(byte* payload, unsigned int length)
@@ -441,10 +490,23 @@ void processCmd(byte* payload, unsigned int length)
     {
       if(param1 == 1) 
       {
-        digitalWrite(LED_PIN,param1);
-        publishStatus(RESPONSE_OK);
+        if(param2 == 1)
+        {
+          digitalWrite(LED_PIN,param2);
+          publishStatus(RESPONSE_OK);
+        }
+        else{digitalWrite(LED_PIN,0);}
       }
-      else          {digitalWrite(LED_PIN,0);}  
+      if(param1 == 2) 
+      {
+        if(param2 ==1)
+        {
+          digitalWrite(LED_STATUS_PIN,param2);
+          publishStatus(RESPONSE_OK);
+        }
+        else{digitalWrite(LED_STATUS_PIN,0);}  
+      }
+      
     }
     else if(strcmp(cmd,"heater_set") == 0) // SET HEATER value: arg1 = heater value
     {
@@ -510,12 +572,17 @@ void processCmd(byte* payload, unsigned int length)
 
 void publishSensor(const char * resp)
 { 
-  const size_t capacity_tx = JSON_ARRAY_SIZE(20) + JSON_OBJECT_SIZE(7);
+  #ifdef PMS5003ST
+    const size_t capacity_tx = JSON_ARRAY_SIZE(20) + JSON_OBJECT_SIZE(11);
+  #else
+    const size_t capacity_tx = JSON_ARRAY_SIZE(20) + JSON_OBJECT_SIZE(9);
+  #endif
   DynamicJsonDocument doc_tx(capacity_tx);
   
   doc_tx["id"]      = SENSOR_ID;
-  doc_tx["time"]    = millis();
   doc_tx["type"]    = TYPE_DATA;
+  doc_tx["time"]    = millis();
+  doc_tx["count"]   = pms_count;
 
   JsonArray data_tx = doc_tx.createNestedArray("data");
   
@@ -524,15 +591,6 @@ void publishSensor(const char * resp)
     data_tx.add(PMS_buff[i]);
   }
   
-  for( uint8_t i = 0; i < MAX_PMS_BUFF_SIZE ;i++)
-  {
-    sht_rh_avg    += SHT_rh_buff[i];
-    sht_temp_avg  += SHT_temp_buff[i];
-    dht_rh_avg    += DHT_rh_buff[i];
-    dht_temp_avg  += DHT_temp_buff[i];
-
-  }
-
   sht_temp  = sht_temp_avg / 20.0;
   sht_rh    = sht_rh_avg   / 20.0;
 
@@ -543,8 +601,13 @@ void publishSensor(const char * resp)
   doc_tx["sht_h"]  = sht_rh;
   doc_tx["dht_t"]  = dht_temp;
   doc_tx["dht_h"]  = dht_rh;
-  doc_tx["state"]  = resp;
-
+  #ifdef PMS5003ST
+    pms_temp  = pms_temp_avg / 20.0;
+    pms_rh    = pms_rh_avg   / 20.0;
+    doc_tx["psm_t"]  = pms_temp;
+    doc_tx["psm_h"]  = pms_rh;
+  #endif
+  
   serializeJson(doc_tx, json_tx);
   SerialDBG.println("msg to publish: ");
   SerialDBG.println(json_tx);
@@ -588,13 +651,52 @@ void IRAM_ATTR timerISR()  //Timer ISR
   }
 }
 
+void IRAM_ATTR timer_HM_ISR()  //Timer ISR
+{
+  //update sensor temperature
+  //if temp > limit then stop
+
+}
+
 void readSensors()
 {
-  readPMSdata(&SerialPMS);
-  PMS_buff[pms_idx]       = data.pm25_standard;
-   
-  SHT_rh_buff[pms_idx]    = sht31.readTemperature();
-  SHT_temp_buff[pms_idx]  = sht31.readHumidity();
+  while(!pms_data_ready)
+  {
+    pms_data_ready = readPMSdata(&SerialPMS);
+  }
+
+  if(pms_data_ready)
+  {
+    pms_count+=1;
+    pms_data_ready = false;
+  }
+
+  PMS_buff[pms_idx] = data.pm25_standard;
+
+  sht_rh_avg    -= SHT_rh_buff[pms_idx];
+  sht_temp_avg  -= SHT_temp_buff[pms_idx];
+  dht_rh_avg    -= DHT_rh_buff[pms_idx];
+  dht_temp_avg  -= DHT_temp_buff[pms_idx];
+  
+
+  SHT_rh_buff[pms_idx]    = sht31.readHumidity();
+  SHT_temp_buff[pms_idx]  = sht31.readTemperature();
+  DHT_rh_buff[pms_idx]    = dht.readHumidity();
+  DHT_temp_buff[pms_idx]  = dht.readTemperature();
+
+  sht_rh_avg    += SHT_rh_buff[pms_idx];
+  sht_temp_avg  += SHT_temp_buff[pms_idx];
+  dht_rh_avg    += DHT_rh_buff[pms_idx];
+  dht_temp_avg  += DHT_temp_buff[pms_idx];
+ 
+  #ifdef PMS5003ST
+    pms_rh_avg    -= PMS_rh_buff[pms_idx];
+    pms_temp_avg  -= PMS_temp_buff[pms_idx];
+    PMS_rh_buff[pms_idx]    = data.pm_h;
+    PMS_temp_buff[pms_idx]  = data.pm_t;
+    pms_rh_avg    += PMS_rh_buff[pms_idx];
+    pms_temp_avg  += PMS_temp_buff[pms_idx];
+  #endif
   
   SerialDBG.print("muestra: ");SerialDBG.print(pms_idx);
   SerialDBG.print(" valor: ");SerialDBG.println(PMS_buff[pms_idx]);
@@ -602,6 +704,7 @@ void readSensors()
 
 bool readPMSdata(Stream *s) 
 {
+#ifdef PMS5003
   if (!s->available()) 
   {
     return false;
@@ -648,6 +751,51 @@ bool readPMSdata(Stream *s)
   }
   // success!
   return true;
+#endif
+#ifdef PMS5003ST
+  if (!s->available()) 
+  {
+    return false;
+  }
+  // Read a byte at a time until we get to the special '0x42' start-byte
+  if (s->peek() != 0x42) 
+  {
+    s->read();
+    return false;
+  }
+  // Now read all 32 bytes
+  if (s->available() < 40) 
+  {
+    return false;
+  }
+
+  uint8_t buffer[40];
+  uint16_t sum = 0;
+  s->readBytes(buffer, 40);
+  // get checksum ready
+  for(uint8_t i=0; i<38; i++) 
+  {
+    sum += buffer[i];
+  }
+  // The data comes in endian'd, this solves it so it works on all platforms
+  uint16_t buffer_u16[19];
+  for(uint8_t i=0; i<19; i++)
+  {
+    buffer_u16[i] = buffer[2 + i*2 + 1];
+    buffer_u16[i] += (buffer[2 + i*2] << 8);
+  }
+
+  // put it into a nice struct :)
+  memcpy((void *)&data, (void *)buffer_u16, 38);
+
+  if(sum != data.checksum)
+  {
+    // Serial.println("Checksum failure");
+    return false;
+  }
+  // success!
+  return true;
+#endif
 }
 
 bool setPowerBoostKeepOn(int en)
