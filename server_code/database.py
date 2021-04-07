a2af10fcbf58dbadc97f70bb94ef6b0bd17738b7
NoelGaspar <waaraya@uc.cl>
2021-04-06 23:21:52 -0400

    testing on posting in austral db

--- server_code/database.py
+++ server_code/database.py
@@ -1,24 +1,20 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  user = 'root',
-  passwd = 'Efmmp_2019!',
-  host = 'localhost',
-  database = 'nodes',
+  #user = 'root',
+  #passwd = 'Efmmp_2019!',
+  #host = 'localhost',
+  #database = 'nodes',
+  user      = 'wladimir',
+  passwd    = 'Wladimir123-',
+  host      = '190.121.23.218',
+  database  = 'filtro',
   )
 
 print(mydb)
 
 my_cursor = mydb.cursor()
-post_CMD  = "INSERT INTO ciep_test (node_id,time_,count,sht_t,sht_h,dht_t,dht_h,pms_25,pms_t,pms_h) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-test1     = ("em-ciep-00",0,20,25.5,55.5,25.5,55.5,12,25.5,55.5)
-
-post_test = "INSERT INTO test (id_nodo,temp,cknumb) VALUES (%s,%s,%s)"
-test_data = ("em-ciep-00",25.5,20)
-
-
+post_CMD  = "INSERT INTO test (id,time,pm25,tn,hn,ts,hs) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+test1     = ("CIEP-00",1,20.0,25.5,55.5,25.5,55.5)
 my_cursor.execute(post_CMD,test1)
-#my_cursor.execute(post_test,test_data)
 mydb.commit()
-
-
--- server_code/server_mqtt.py
+++ server_code/server_mqtt.py
@@ -2,7 +2,7 @@
 
 import paho.mqtt.client as mqtt
 import json
-import csv
+#import csv
 import sys
 import signal
 import mysql.connector
@@ -12,28 +12,28 @@ import datetime
 
 #MQTT BROKER
 MQTT_BROKER_ADDR  = "190.121.23.217"
-MQTT_BROKER_PORT  = 1883
-CHANNEL_SERVER_RX = "pm/test1"
-CHANNEL_SERVER_TX = "pm/test2"
-#CHANNEL_SERVER_RX = "pmt/test1"
-#CHANNEL_SERVER_TX = "pmt/test2"
-
-
+MQTT_BROKER_PORT  =  1883
+CHANNEL_SERVER_RX = "CIEP/tx"
+CHANNEL_SERVER_TX = "CIEP/rx"
 
 #DATA BASE
-USER      = 'root'
-PASSWD    = 'Efmmp_2019!'
-HOST      = 'localhost'
-DATABASE  = 'nodes'
+USER      = 'wladimir'
+PASSWD    = 'Wladimir123-'
+HOST      = '190.121.23.218'
+DATABASE  = 'filtro'
 
 
-dflt_row = "em-ciep-00,0,20,25.5,55.5,25.5,55.5,12,25.5,55.5"
-dflt_msg = "{\"id\":\"em-ciep-01\", \"type\":\"data\", \"time\": 0, \"count\": 0, \"data\": [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0], \"sht_t\": 29.78752, \"sht_h\": 38.06948, \"dht_t\": 25.5, \"dht_h\": 25.5, \"psm_t\": 296.7, \"psm_h\": 299.85}"
+
+# mosquitto_pub -h "190.121.23.217" -t "CIEP/tx" -m "{\"id\":\"CIEP-00\", \"type\":\"data\", \"time\": 1511118471, \"pm25\": 34.0, \"tn\": 29.7, \"hn\": 38.06948, \"ts\": 25.5, \"hs\": 25.5}"
+
+dflt_row = "CIEP-00,0,20,25.5,55.5,25.5,55.5"
+dflt_msg = "{\"id\":\"CIEP-00\", \"type\":\"data\", \"time\": 1511118471, \"pm25\": 34.0, \"tn\": 29.7, \"hn\": 38.06948, \"ts\": 25.5, \"hs\": 25.5}"
 dflt_data = json.loads(dflt_msg)
 
-#CSV
-path_csv = "data/"
-
+'''
+Comment:      This function recive a json dictionary and convert it to a row of csv file.
+Description:  Join as string all values's item in a given dictionary.
+'''
 def json2csv(d):
 
   row = ""
@@ -42,28 +42,26 @@ def json2csv(d):
   row = ",".join(z)
   return row
 
+'''
+Comment:      This function recive a json dictionary and convert it to a record format
+              to post it in a mysql db.
+Description:  Join as tuple all values's item in a json given as dictionary. 
+'''
 def json2db(d):
   row_list = []
   for key in d:
     if key != "type":
-      if key == "data":  
-        row_list.append(str(d[key][0]))
-      else:
-        row_list.append(str(d[key]))
-  row = ",".join(row_list)
-  print(row)
+      row_list.append(str(d[key]))
   return tuple(row_list)
 
 def signal_handler(signal, frame):
   print("exiting...")
-  file.close()
-  print("file closed")
   sys.exit(0)
 
 def connected(client, userdata, flag , rc):
   print("server connected")
   client.subscribe(CHANNEL_SERVER_RX)
-  client.publish(CHANNEL_SERVER_TX, "HELLO WORD")
+  client.publish(CHANNEL_SERVER_TX, "Server connected")
 
 def rx_callback(client, userdata, msg):
   print("mensaje recibido : ", end="")
@@ -71,21 +69,9 @@ def rx_callback(client, userdata, msg):
 
   data = json.loads(msg.payload)  #convert payload to a dic
   
-  if data['type'] == "data":  #check if the type of msg is "data"
-    try:
-      for key in data: # patch for null data.
-        if data[key] is None:
-          data[key] = 0.0                    
-      print("guardando local:", end="")
-      print(data)
-      writer.writerow(data)
-    except:
-      writer.writerow(dflt_data)
-
-    db_row = json2db(data)
-
-    my_cursor.execute(post_db_data,db_row)
-    mydb.commit()
+  db_row = json2db(data)
+  my_cursor.execute(post_db_data,db_row)
+  mydb.commit()
     
 
 if __name__ == '__main__':
@@ -94,30 +80,16 @@ if __name__ == '__main__':
   print("connecting to mySQL database")
   mydb = mysql.connector.connect(user = USER, passwd = PASSWD,host = HOST,database = DATABASE)
   my_cursor = mydb.cursor()
-  post_db_data  = "INSERT INTO ciep_test (node_id,time_,count,pms_25,sht_t,sht_h,dht_t,dht_h,pms_t,pms_h) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-
-  print("open csv file")
-  #default_json = '{"id":"node_nn", "type":"data","time": 0,"count":0,"data":0,"sht_t":0.0,"sht_t":0.0,"sht_h":0.0,"dht_t":0.0,"dht_h":0.0,"psm_t":0.0,"psm_h":0.0}'
-  #default_dic = json.loads(default_json)
-
-  #mosquitto_pub -h "190.121.23.217" -t "CIEP/TX" -m "{\"id\":\"node_nn\", \"type\":\"data\",\"time\": 0,"count":0,\"data\": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\"sht_t\":0.0,\"sht_t\":0.0,\"sht_h\":0.0,\"dht_t\":0.0,\"dht_h\":0.0,\"pms_t\":0.0,\"pms_h\":0.0}" 
-  #header      = default_dic.keys() 
-  header      = dflt_data.keys() 
-  filename    = path_csv+datetime.now().strftime("%d%m%y_%H%M%S")+".csv"
-  file        = open(filename,"w",newline='')
-  writer      = csv.DictWriter(file,fieldnames = header,lineterminator = '\n')
-  writer.writeheader()
-
+  post_db_data  = "INSERT INTO test (id,time,pm25,tn,hn,ts,hs) VALUES (%s,%s,%s,%s,%s,%s,%s)"
 
   print("connecting to mqtt client")  
   client = mqtt.Client("server_test")
   client.on_message = rx_callback      #attach to callback function
   client.on_connect = connected        #attach initial subscribe test
-  
-  
+    
+
   #client connect
   client.connect(MQTT_BROKER_ADDR,MQTT_BROKER_PORT,60)
-
   signal.signal(signal.SIGINT,signal_handler)
 
   while True:
